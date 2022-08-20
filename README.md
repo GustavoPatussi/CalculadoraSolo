323a4f4e69bb1263a5ec525c676c68cb1ccfee70
GustavoPatussi <gustavopatussibr@gmail.com>
2022-08-20 11:48:47 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-platform-di3e2t
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-di3e2t)
---
+++ main.js
@@ -0,0 +1,78 @@
+var acerto = false;
+var numDisplay = '';
+var DisplayDiv = document.getElementById('display');
+var iAntes;
+function display(i) {
+  if (DisplayDiv.innerText == 0) {
+    if (i == '.') {
+      numDisplay = '0';
+      numDisplay += i;
+      DisplayDiv.innerText = numDisplay;
+      return;
+    } else {
+      if (numDisplay.indexOf('.') > 0) {
+        numDisplay += i;
+        DisplayDiv.innerText = numDisplay;
+        return;
+      }
+      numDisplay = i;
+      DisplayDiv.innerText = numDisplay;
+    }
+  } else {
+    if (numDisplay.indexOf('.') > 0) {
+      if (i == '.') {
+        if (acerto) {
+          numDisplay += i;
+          DisplayDiv.innerText = numDisplay;
+          acerto = false;
+        }
+        return;
+      }
+    }
+    numDisplay += i;
+    DisplayDiv.innerText = numDisplay;
+  }
+}
+function operacoes(i) {
+  if (DisplayDiv.innerText == 0) {
+    return;
+  }
+  if (numDisplay.indexOf(iAntes) > 0) {
+    numDisplay = numDisplay.replace(iAntes, i);
+    DisplayDiv.innerText = numDisplay;
+    iAntes = i;
+    return;
+  } else {
+    numDisplay += i;
+    DisplayDiv.innerText += i;
+    iAntes = i;
+    acerto = true;
+  }
+}
+function apagarDisplay() {
+  DisplayDiv.innerHTML = '0';
+  numDisplay = '';
+  acerto = false;
+}
+function calcularResultado() {
+  var pt1 = numDisplay.substring(0, numDisplay.indexOf(iAntes));
+  var pt2 = numDisplay.substring(
+    numDisplay.indexOf(iAntes) + 1,
+    numDisplay.length
+  );
+  if (iAntes == '+') {
+    var resultado = parseFloat(pt1) + parseFloat(pt2);
+  }
+  if (iAntes == '-') {
+    var resultado = parseFloat(pt1) - parseFloat(pt2);
+  }
+  if (iAntes == '*') {
+    var resultado = parseFloat(pt1) * parseFloat(pt2);
+  }
+  if (iAntes == '/') {
+    var resultado = parseFloat(pt1) / parseFloat(pt2);
+  }
+  acerto = false;
+  numDisplay = String(resultado);
+  DisplayDiv.innerText = numDisplay;
+}
